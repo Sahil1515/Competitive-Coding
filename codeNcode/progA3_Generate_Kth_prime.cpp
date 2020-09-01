a6266dca57484edab737a8129eebfa8fe962a235
Sahil <sahilsainisalaria@gmail.com>
2020-09-01 20:12:20 +0530

    codeNcode

--- codeNcode/progA3_Generate_Kth_prime.cpp
+++ codeNcode/progA3_Generate_Kth_prime.cpp
@@ -1,38 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 vector<int> v;
 
 void sieve()
 {
-    int n= 10000000; 
-    bool arr[n+1]={true};
-    arr[0]=arr[1]=false;
+    int n = 10000000;
+    bool arr[n + 1] = {true};
+    arr[0] = arr[1] = false;
 
-    for(int i=2;i*i<n; i++)
+    for (int i = 2; i * i < n; i++)
     {
-        int p=i*i;
-        for(int j=p;j<=n; j+=i)
+        int p = i * i;
+        for (int j = p; j <= n; j += i)
         {
-            arr[j]=false;
+            arr[j] = false;
         }
     }
-    for(int i=2;i*i<=n;i++)
+    for (int i = 2; i * i <= n; i++)
     {
-        if(arr[i])
-        v.push_back(i);
+        if (arr[i])
+            v.push_back(i);
     }
-
 }
 int main()
 {
     int q;
-    cin>>q;
+    cin >> q;
     sieve();
-    while(q--)
+    int k;
+    while (q--)
     {
-        
+        scanf("%d", &k);
+        printf("%d\n", v[k]);
     }
-
+    return 0;
 }
