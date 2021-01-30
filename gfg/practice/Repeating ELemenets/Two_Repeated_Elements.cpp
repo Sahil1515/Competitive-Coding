d1cf7c4cc6fdc2f92c28b60b5f2bb3970f455fe6
sahil <sahilsainisalaria@gmail.com>
2021-01-30 12:51:57 +0530

    cpp

---
+++ gfg/practice/Repeating ELemenets/Two_Repeated_Elements.cpp
@@ -0,0 +1,13 @@
+// You are given an array of N+2 integer elements. 
+// All elements of the array are in range 1 to N. 
+// Also, all elements occur once except two numbers 
+// which occur twice. Find the two repeating numbers.
+
+// Example 1:
+
+// Input:
+// N = 4
+// array[] = {1,2,1,3,4,3}
+// Output: 1 3
+// Explanation: In the given array, 
+// 1 and 3 are repeated two times.
